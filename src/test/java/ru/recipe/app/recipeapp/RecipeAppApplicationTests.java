12b64aaf3c83f220225b43fc5683c22825f43b97
Konstantin <kdanilov1911@gmail.com>
2022-12-30 21:44:03 +0300

    first commit

---
+++ src/test/java/ru/recipe/app/recipeapp/RecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.recipe.app.recipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
