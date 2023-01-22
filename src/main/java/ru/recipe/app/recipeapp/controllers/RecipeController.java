76cfd8fb86d46bf2d73aa34cd8d143d117d7b0b1
Konstantin <kdanilov1911@gmail.com>
2023-01-22 22:43:28 +0300

    HW is done

--- src/main/java/ru/recipe/app/recipeapp/controllers/RecipeController.java
+++ src/main/java/ru/recipe/app/recipeapp/controllers/RecipeController.java
@@ -1,12 +1,20 @@
 package ru.recipe.app.recipeapp.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.recipe.app.recipeapp.model.Recipe;
 import ru.recipe.app.recipeapp.services.RecipeService;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("/recipe")
+@Tag(name = "Констроллер рецептов", description = "CRUD-опреции для работы с рецептами")
 public class RecipeController {
 
     private final RecipeService recipeService;
@@ -15,28 +23,33 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
+    @Operation(
+            summary = "Добавление рецепта в мапу",
+            description = "Необходимо передать с помощью JSON объект рецепта"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Рецепт добавлен"
+            )
+    })
     @PostMapping("/add")
-    public ResponseEntity<Recipe> addRecipe(@RequestBody Recipe recipe) {
+    public ResponseEntity<Recipe> addRecipe(@Valid @RequestBody Recipe recipe) {
         recipeService.addRecipe(recipe);
         return ResponseEntity.ok(recipe);
     }
 
-    @PostMapping("/edit/{id}")
-    public ResponseEntity<Recipe> editRecipe (@PathVariable int id, @RequestBody Recipe recipe) {
+    @PostMapping("/{id}")
+    public ResponseEntity<Recipe> editRecipe(@PathVariable int id, @Valid @RequestBody Recipe recipe) {
         recipeService.editRecipe(id, recipe);
         return ResponseEntity.ok().body(recipe);
     }
 
-    @DeleteMapping("/delete/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Recipe> deleteRecipe(@PathVariable int id) {
         recipeService.deleteRecipe(id);
         return ResponseEntity.ok().build();
     }
-
-
-
-
-
 
 
 }
--- src/main/java/ru/recipe/app/recipeapp/model/Ingredients.java
+++ src/main/java/ru/recipe/app/recipeapp/model/Ingredients.java
@@ -2,53 +2,17 @@ package ru.recipe.app.recipeapp.model;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.PositiveOrZero;
+
 @Data
 public class Ingredients {
 
+    @NotBlank
     private String name;
+    @PositiveOrZero
     private int count;
+    @NotBlank
     private String measure;
 
-    public Ingredients(String name, int count, String measure) {
-        setName(name);
-        setCount(count);
-        this.measure = measure;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        if (name != null && !name.isBlank()) {
-            this.name = name;
-        } else {
-            this.name = "Default";
-        }
-
-    }
-
-    public int getCount() {
-        return count;
-    }
-
-    public void setCount(int count) {
-        if (count >= 0) {
-            this.count = count;
-        } else {
-            this.count = -1;
-        }
-    }
-
-    public String getMeasure() {
-        return measure;
-    }
-
-    public void setMeasure(String measure) {
-        if (measure != null && !measure.isBlank()) {
-            this.measure = measure;
-        } else {
-            this.measure = "Default";
-        }
-    }
 }
